cbdfa7c78d35ae446f1994c20ba8844dc4fc02ad
miki <ty@adres.pl>
2018-06-08 21:32:16 +0200

    aktualizacja

--- shelf/admin.py
+++ shelf/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+#from .models import * #nie robic tak
+from .models import Author, Publisher, Book
+
+class AuthorAdmin(admin.ModelAdmin):
+	search_fields = ['last_name', 'first_name']
+	ordering = ['last_name']
+	pass
+
+class BookAdmin(admin.ModelAdmin):
+	search_fields = ['title']
+	list_display = ['title','author','isbn','publisher']
+
+
+admin.site.register(Book,BookAdmin)
+
+admin.site.register(Author,AuthorAdmin)	
+
+admin.site.register([Publisher,])
+
 # Register your models here.
